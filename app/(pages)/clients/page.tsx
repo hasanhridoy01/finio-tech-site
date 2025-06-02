82bf2201419600ed512f28b0a1a469d4ba784fa3
Mozammel Haque <mozammel@newroztech.com>
2025-06-02 12:37:08 +0600

    create client page

---
+++ app/(pages)/clients/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="container mx-auto md:pt-44 pt-28 md:px-0 px-5 h-auto">
+        <div className="my-10">
+          <h1 className="section-title md:leading-[55px] leading-8 md:text-[48px] text-[24px] mb-2 text-center md:text-start text-white">
+            Clints 
+          </h1>
+          <h3 className="md:text-[18px] text-[14px] font-medium text-[#222222] text-center md:text-start md:w-[850px] w-full">
+            Discover Cutting-Edge Financial Technologies Designed to Streamline
+            Operations, Maximize Efficiency, and Drive Sustainable Growth for
+            Your Business
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
